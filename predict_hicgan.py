443d279df322e407ed38ec98f1a30164db8b1326
Yangyang Hu <yhu017@ucr.edu>
2020-10-07 18:58:52 -0700

    update

--- predict_hicgan.py
+++ predict_hicgan.py
@@ -114,11 +114,6 @@ def save_data(hic, compact, size, file):
 
 def save_data_n(key, hics, compacts, sizes, low_res, out_dir):
     file = os.path.join(out_dir, f'predict_chr{key}_{low_res}.npz')
-    print(key)
-    print(hics.keys())
-    print(compacts)
-    print(sizes)
-    print(file)
     save_data(hics[key], compacts[key], sizes[key], file)
 
 def predict(data_dir, model_name, out_dir, lr=40000, cuda=0):
